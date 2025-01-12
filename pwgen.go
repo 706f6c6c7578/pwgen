d8122f0a2a56a3a4724b3e0390298310b2c54cf2
Stefan <stefan.claas@mail.ru>
2025-01-12 22:57:00 +0100

    Add files via upload

--- pwgen.go
+++ pwgen.go
@@ -10,7 +10,7 @@ import (
 
 const (
     minLength = 13
-    charset   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-+=.,:;"
+    charset   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%&/*()-+=.,:;"
 )
 
 func main() {
